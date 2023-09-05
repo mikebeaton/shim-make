90913f38e1da9faf55cf5fc1153e5ec248f10177
Mike Beaton <mjsbeaton@gmail.com>
2023-09-05 12:07:01 +0100

    Building in both

--- shim-make.sh
+++ shim-make.sh
@@ -3,8 +3,11 @@
 # Copyright © 2023 Mike Beaton. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 #
+# Makes own-build shim which launches OpenCore.efi and includes specified vendor certificate lists.
+# Builds on macOS using Ubuntu multipass or on Linux directly (Ubuntu, *TODO* Fedora tested).
+#
 # To debug shim (e.g. within OVMF) make with
-# shim-make.sh make OPTIMIZATIONS="-O0"
+# ./shim-make.sh make OPTIMIZATIONS="-O0"
 # and set 605dab50-e046-4300-abb6-3dd810dd8b23:SHIM_DEBUG to (UINT8)1 (<data>AQ==</data>) in NVRAM
 #
 
@@ -18,13 +21,33 @@ unamer() {
   fi
 }
 
+shim_command () {
+    DIR=$1
+    shift
+    if [ $DARWIN -eq 1 ] ; then
+        if [ $ECHO -eq 1 ] ; then
+            echo multipass exec ${OC_SHIM} --working-directory "'${DIR}'" -- $@ 1>/dev/stderr
+        fi
+        eval multipass exec ${OC_SHIM} --working-directory "'${DIR}'" -- $@
+    else
+        if [ $ECHO -eq 1 ] ; then
+            echo "[${DIR}]" $@ 1>/dev/stderr
+        fi
+        pushd "${DIR}" 1>/dev/null
+        eval $@
+        retval=$?
+        popd 1>/dev/null
+        return $retval
+    fi
+}
+
 usage() {
     echo "Usage:"
     echo " ./${SELFNAME} [args] [setup|clean|make [options]|install [esp-root-path]|mount [multipass-path]]"
     echo
     echo "Args:"
-    echo "  -r : Specify shim output root, default" ~/shim_root
-    echo "  -s : Specify shim source location, default" ~/OpenSource/shim
+    echo "  -r : Specify shim output root, default '${ROOT}'"
+    echo "  -s : Specify shim source location, default '${SHIM}'"
     echo "If used -r/-s:"
     echo " - Should be specified on every call, they are not remembered from setup"
     echo " - Should be specified before the ${SELFNAME} command"
@@ -36,15 +59,14 @@ usage() {
     if [ $DARWIN -eq 1 ] ; then
         LOCATION="${ROOT}"
     fi
-    echo -n "  ./${SELFNAME} make VENDOR_DB_FILE=${LOCATION}/combined/vendor.db VENDOR_DBX_FILE=${LOCATION}/combined/vendor.dbx (makes shim with db and dbx contents"
+    echo -n "  ./${SELFNAME} make VENDOR_DB_FILE='${LOCATION}/combined/vendor.db' VENDOR_DBX_FILE='${LOCATION}/combined/vendor.dbx' (makes shim with db and dbx contents"
     if [ $DARWIN -eq 1 ] ; then
         echo -n "; note VENDOR_DB_FILE and VENDOR_DBX_FILE are inside a directory shared with VM"
     fi
     echo ")"
-    echo "  ./${SELFNAME} install /Volumes/EFI (installs made files to ESP mounted at /Volumes/EFI)"
+    echo "  ./${SELFNAME} install '${EXAMPLE_ESP}' (installs made files to ESP mounted at '${EXAMPLE_ESP}')"
     echo
     echo "After installation shimx64.efi and mmx64.efi must be signed by user ISK; OpenCore.efi must have an .sbat section added and be signed by user ISK."
-    echo ""
 }
 
 mount_path () {
@@ -58,54 +80,59 @@ mount_path () {
         DEST=${SRC}
     fi
 
-    if [ ! -d ${SRC} ] ; then
+    if [ ! -d "${SRC}" ] ; then
         echo "Adding ${SRC}..."
-        mkdir ${SRC}
+        mkdir "${SRC}"
     else
         echo "${SRC} already present..."
     fi
 
     if ! multipass info ${OC_SHIM} | grep "${DEST}" 1>/dev/null ; then
         echo "Mounting ${SRC}..."
-        multipass mount ${SRC} ${OC_SHIM}:${DEST}
+        multipass mount "${SRC}" "${OC_SHIM}:${DEST}"
     else
         echo "${SRC} already mounted..."
     fi
 }
 
 get_ready () {
-    if ! command -v multipass 1>/dev/null ; then
-        echo "Installing Ubuntu multipass..."
-        brew install --cask multipass || exit 1
-    else
-        echo "Ubuntu multipass already installed..."
+    if [ $DARWIN -eq 1 ] ; then
+        if ! command -v multipass 1>/dev/null ; then
+            echo "Installing Ubuntu multipass..."
+            brew install --cask multipass || exit 1
+        else
+            echo "Ubuntu multipass already installed..."
+        fi
+
+        if ! multipass info ${OC_SHIM} &>/dev/null ; then
+            echo "Launching ${OC_SHIM} multipass instance..."
+            multipass launch -n ${OC_SHIM} || exit 1
+        else
+            echo "${OC_SHIM} multipass instance already launched..."
+        fi
+
+        mount_path "${ROOT}"
     fi
 
-    if ! multipass info ${OC_SHIM} &>/dev/null ; then
-        echo "Launching ${OC_SHIM} multipass instance..."
-        multipass launch -n ${OC_SHIM} || exit 1
-    else
-        echo "${OC_SHIM} multipass instance already launched..."
-    fi
-
-    mount_path ${ROOT}
+    # Make sure parent directory exists
+    #shim_command . sudo mkdir -p "'$(dirname "${SHIM}")'" || exit 1
 
     #
-    # For debug/develop purposes it would be nicer to keep the source code in macOS,
-    # just mounted and built in multipass, but the build is about 1/3 the speed of
+    # For debug/develop purposes on Darwin it would be nicer to keep the source code in
+    # macOS, just mounted and built in multipass, but the build is about 1/3 the speed of
     # building in a native multipass directory.
     # For the purposes of having a fast build but code which can be opened e.g.
     # within an IDE within macOS, sshfs can be used to mount out from multipass
-    # to macOS:
+    # to macOS using ./shim-make mount:
     #  - https://github.com/canonical/multipass/issues/1070
     #  - https://osxfuse.github.io/
     #
-    if ! multipass exec ${OC_SHIM} -- test -d ${SHIM} ; then
+    if ! shim_command . test -d "'${SHIM}'"  ; then
         echo "Cloning rhboot/shim..."
-        multipass exec ${OC_SHIM} -- git clone https://github.com/rhboot/shim.git ${SHIM} || exit 1
-        multipass exec ${OC_SHIM} --working-directory ${SHIM} -- git submodule update --init || exit 1
+        shim_command . git clone https://github.com/rhboot/shim.git "'${SHIM}'"  || exit 1
+        shim_command "${SHIM}" git submodule update --init || exit 1
     else
-        if ! multipass exec ${OC_SHIM} --working-directory ${SHIM} -- git remote -v | grep "rhboot/shim" 1>/dev/null ; then
+        if ! shim_command "${SHIM}" git remote -v | grep "rhboot/shim" 1>/dev/null ; then
             echo "FATAL: VM subdirectory ${SHIM} is already present, but does not contain rhboot/shim!"
             exit 1
         fi
@@ -115,52 +142,58 @@ get_ready () {
     echo "Make.defaults:"
 
     # These two modifications to Make.defaults only required for debugging
-    FOUND=$(multipass exec ${OC_SHIM} --working-directory ${SHIM} -- grep "gdwarf" Make.defaults | wc -l)
+    FOUND=$(shim_command "${SHIM}" grep "gdwarf" Make.defaults | wc -l)
     if [ $FOUND -eq 0 ] ; then
         echo "  Updating gdwarf flags..."
-        multipass exec ${OC_SHIM} --working-directory ${SHIM} -- sed -i 's^-ggdb \\^-ggdb -gdwarf-4 -gstrict-dwarf \\^g' Make.defaults
+        shim_command "${SHIM}" sed -i "'s^-ggdb \\\\^-ggdb -gdwarf-4 -gstrict-dwarf \\\\^g'" Make.defaults
     else
         echo "  gdwarf flags already updated..."
     fi
 
-    FOUND=$(multipass exec ${OC_SHIM} --working-directory ${SHIM} -- grep "${ROOT}" Make.defaults | wc -l)
+    FOUND=$(shim_command "${SHIM}" grep "'${ROOT}'" Make.defaults | wc -l)
     if [ $FOUND -eq 0 ] ; then
         echo "  Updating debug directory..."
-        multipass exec ${OC_SHIM} --working-directory ${SHIM} -- sed -i s^-DDEBUGDIR=\'L\"/usr/lib/debug/usr/share/shim/$\(ARCH_SUFFIX\)-$\(VERSION\)$\(DASHRELEASE\)/\"\'^-DDEBUGDIR=\'L\"${ROOT}/usr/lib/debug/boot/efi/EFI/OC/\"\'^g Make.defaults
+        shim_command "${SHIM}" sed -i "\"s^-DDEBUGDIR='L\\\"/usr/lib/debug/usr/share/shim/\\\$(ARCH_SUFFIX)-\\\$(VERSION)\\\$(DASHRELEASE)/\\\"'^-DDEBUGDIR='L\\\"${ROOT}/usr/lib/debug/boot/efi/EFI/OC/\\\"'^g\"" Make.defaults
     else
         echo "  Debug directory already updated..."
     fi
 
     # Work-around for https://github.com/rhboot/shim/issues/596
-    FOUND=$(multipass exec ${OC_SHIM} --working-directory ${SHIM} -- grep "export DEFINES" Make.defaults | wc -l)
+    FOUND=$(shim_command "${SHIM}" grep "'export DEFINES'" Make.defaults | wc -l)
     if [ $FOUND -eq 0 ] ; then
         echo "  Updating exports..."
         # var assignment to make output piping work normally
-        temp=$(echo "export DEFINES" | multipass exec ${OC_SHIM} --working-directory ${SHIM} -- tee -a Make.defaults) 1>/dev/null
+        temp=$(echo "export DEFINES" | shim_command "${SHIM}" tee -a Make.defaults) 1>/dev/null
     else
         echo "  Exports already updated..."
     fi
 
-    FOUND=$(multipass exec ${OC_SHIM} -- command -v gcc | wc -l)
+    FOUND=$(shim_command . command -v gcc | wc -l)
     if [ $FOUND -eq 0 ] ; then
         echo "Installing dependencies..."
-        multipass exec ${OC_SHIM} -- sudo apt-get update
-        multipass exec ${OC_SHIM} -- sudo apt install -y gcc make git libelf-dev
+        shim_command . sudo apt-get update
+        shim_command . sudo apt install -y gcc make git libelf-dev
     else
         echo "Dependencies already installed..."
     fi
 }
 
-ROOT=~/shim_root
-SHIM=~/OpenSource/shim
+# ROOT=~/shim_root
+# SHIM=~/OpenSource/shim
+ROOT=~/shim\ root
+SHIM=~/Open\ Source/my\ shim
 OC_SHIM=oc-shim
 
 SELFNAME="$(/usr/bin/basename "${0}")"
 
-DARWIN=0
+ECHO=0
 
 if [ "$(unamer)" = "Darwin" ] ; then
     DARWIN=1
+    EXAMPLE_ESP='/Volumes/EFI'
+else
+    DARWIN=0
+    EXAMPLE_ESP='/boot/efi'
 fi
 
 OPTS=0
@@ -182,6 +215,9 @@ while [ "${1:0:1}" = "-" ] ; do
         else
             echo "No shim directory specified" && exit 1
         fi
+    elif [ "$1" = "--echo" ] ; then
+        ECHO=1
+        shift
     else
         echo "Unknown option: $1"
         exit 1
@@ -194,17 +230,17 @@ if [ "$1" = "setup" ] ; then
     exit 0
 elif [ "$1" = "clean" ] ; then
     echo "Cleaning..."
-    multipass exec ${OC_SHIM} --working-directory ${SHIM} -- make clean
+    shim_command "${SHIM}" make clean
     exit 0
 elif [ "$1" = "make" ] ; then
     echo "Making..."
     shift
-    multipass exec ${OC_SHIM} --working-directory ${SHIM} -- make DEFAULT_LOADER="\\\\\\\\OpenCore.efi" OVERRIDE_SECURITY_POLICY=1 "$@"
+    shim_command "${SHIM}" make DEFAULT_LOADER="\\\\\\\\OpenCore.efi" OVERRIDE_SECURITY_POLICY=1 "$@"
     exit 0
 elif [ "$1" = "install" ] ; then
     echo "Installing..."
-    rm -rf ${ROOT}/usr
-    multipass exec ${OC_SHIM} --working-directory ${SHIM} -- DESTDIR=${ROOT} EFIDIR="OC" OSLABEL="OpenCore" make install
+    rm -rf "'${ROOT}/usr'"
+    shim_command "${SHIM}" DESTDIR="'${ROOT}'" EFIDIR='OC' OSLABEL='OpenCore' make install
     if [ ! "$2" = "" ] ; then
         echo "Installing to ESP ${2}..."
         cp ${ROOT}/boot/efi/EFI/OC/* ${2}/EFI/OC || exit 1
@@ -213,7 +249,7 @@ elif [ "$1" = "install" ] ; then
 elif [ "$1" = "mount" ] ; then
     MOUNT="$2"
     if [ "${MOUNT}" = "" ] ; then
-        MOUNT="${SHIM}"
+        MOUNT=$SHIM
     fi
 
     #
